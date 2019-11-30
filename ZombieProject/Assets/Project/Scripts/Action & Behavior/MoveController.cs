8a22277f8278245b1b0d26f220ed68be213ca1e0
조영래 <choyr100@gmail.com>
2019-11-30 20:58:56 +0900

    test

--- ZombieProject/Assets/Project/Scripts/Action & Behavior/MoveController.cs
+++ ZombieProject/Assets/Project/Scripts/Action & Behavior/MoveController.cs
@@ -68,11 +68,24 @@ public class MoveController : MonoBehaviour
 
         if (currentEquipedItem.m_ItemStat.m_isRayAttack)
         {
-            GameObject newBulletObj = Instantiate(Resources.Load<GameObject>("Prefabs/Weapon/Bullet/" + currentEquipedItem.m_ItemStat.m_BulletString));
-            Bullet newBullet = newBulletObj.GetComponent<Bullet>();
+            Camera cam = Camera.main;
 
-            if (newBullet)
-                newBullet.FireBullet(m_FirePos.transform.position + Vector3.up * 0.1f, m_Character.transform.forward, currentEquipedItem.m_ItemStat.m_BulletSpeed);
+            Debug.Log("ss");
+
+            Vector3 point = new Vector3();
+            Vector2 mousePos = new Vector2();
+            mousePos = Input.mousePosition;
+
+            m_Ray = cam.ScreenPointToRay(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+
+            if (Physics.Raycast(m_Ray, out m_RayCastHit, 50.0f, 1 << LayerMask.NameToLayer("Ground")))
+            {
+                Vector3 hitPoint = m_RayCastHit.point;
+                hitPoint.y = m_Character.transform.position.y;
+                Attack(m_RayCastHit.point, (hitPoint - m_Character.transform.position).normalized);
+
+
+            }
         }
         else
         {

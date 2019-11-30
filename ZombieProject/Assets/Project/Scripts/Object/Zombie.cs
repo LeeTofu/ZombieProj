97f364afb260c220e21bf130203e94b6cb19d304
이성호 <jrho1234@gmail.com>
2019-12-01 02:33:32 +0900

    zombietestcode

--- ZombieProject/Assets/Project/Scripts/Object/Zombie.cs
+++ ZombieProject/Assets/Project/Scripts/Object/Zombie.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Zombie : MovingObject
 {
+    protected BehaviorNode m_zombieBehavior;
+
     // Start is called before the first frame update
-    
+
     public override void Initialize(GameObject _Model, MoveController _Controller)
     {
         if(m_Animator == null)
@@ -14,5 +16,31 @@ public class Zombie : MovingObject
         }
 
         // Test //
+        SelectorNode sel = new SelectorNode();
+
+        SequenceNode seqAtk = new SequenceNode();
+        seqAtk.InsertAction(new ZombieAttackCondition());
+        seqAtk.InsertAction(new ZombieAttackAction());
+
+        sel.InsertAction(seqAtk);
+
+        SequenceNode seqWalk = new SequenceNode();
+        seqWalk.InsertAction(new ZombieWalkCondition());
+        seqWalk.InsertAction(new ZombieWalkAction());
+
+        sel.InsertAction(seqWalk);
+
+        sel.InsertAction(new ZombieIdleAction());
+
+        BehaviorManger.Instance.AddBehaviorNode("TestZombieNode", sel);
+
+        m_zombieBehavior = sel;
+    }
+
+    private void Update()
+    {
+        if (m_zombieBehavior == null) return;
+
+        m_zombieBehavior.Tick();
     }
 }

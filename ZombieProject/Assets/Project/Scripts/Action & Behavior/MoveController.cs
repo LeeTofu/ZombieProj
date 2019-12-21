091a2f64f4e8b725fbcb9df0cd3089dd8c4bb01d
조영래 <choyr100@gmail.com>
2019-12-21 14:32:16 +0900

    move

--- ZombieProject/Assets/Project/Scripts/Action & Behavior/MoveController.cs
+++ ZombieProject/Assets/Project/Scripts/Action & Behavior/MoveController.cs
@@ -26,7 +26,28 @@ public class MoveController : MonoBehaviour
         if (m_Character != null) m_Character.m_Animator.applyRootMotion = false;
     }
 
+    //private void Update()
+    //{
+    //    if (m_InputContoller != null) InputMove();
+    //}
 
+    //private void InputMove()
+    //{
+    //    Vector3 vec3 = m_InputContoller.GetDirectionVec3();
+    //    if(vec3.x != 0 && vec3.y != 0 && m_InputContoller.GetisHit())
+    //    {
+    //        Camera cam = Camera.main;
+    //        vec3 = cam.transform.InverseTransformVector(new Vector3(vec3.x, 0, vec3.y));
+
+    //        transform.rotation = Quaternion.LookRotation(new Vector3(vec3.x, 0, vec3.y));
+
+    //        float length = m_InputContoller.GetLength();
+    //        float limitlength = m_InputContoller.GetLimitedLength();
+    //        if (length >= limitlength) length = limitlength;
+    //        transform.position += transform.forward * Time.deltaTime * length / 10f;
+
+    //    }
+    //}
 
     public InputContoller GetInputContoller()
     {

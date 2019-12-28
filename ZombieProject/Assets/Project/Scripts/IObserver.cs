1142710be628b3653e4a984b8d01728cf8db3542
조영래 <choyr100@gmail.com>
2019-12-28 22:31:47 +0900

    no message

---
+++ ZombieProject/Assets/Project/Scripts/IObserver.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IObserver
+{
+    void ObsUpdate();
+    void BeginDragUpdate();
+    void EndDragUpdate();
+    void DragUpdate();
+}

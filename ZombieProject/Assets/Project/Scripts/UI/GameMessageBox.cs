57c4f3b35e2d37f71d7fb688ddd91811bb9ba762
박정인 <qnpfrehdtks@gmail.com>
2020-01-27 03:21:36 +0900

    no message

---
+++ ZombieProject/Assets/Project/Scripts/UI/GameMessageBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMessageBox : MonoBehaviour
+{
+    [SerializeField]
+    TMPro.TextMeshProUGUI m_Text;
+
+    public void SetString(string _str)
+    {
+        m_Text.text = _str;
+    }
+
+
+    public void ClickButton()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
